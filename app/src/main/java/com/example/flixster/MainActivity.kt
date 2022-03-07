4ad8d7b3012f4beb5876b71280d51bd456b985e1
Austin Tang <austinttang@gmail.com>
2022-03-06 22:41:57 -0500

    Add a detailed page for each movie, including a YouTube player and a rating bar

--- app/src/main/java/com/example/flixster/MainActivity.kt
+++ app/src/main/java/com/example/flixster/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.flixster
 
-import android.content.res.Configuration
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
 import org.json.JSONException
+import androidx.fragment.app.commit
+
 
 private const val TAG = "MainActivity"
 private const val NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
@@ -28,7 +32,15 @@ class MainActivity : AppCompatActivity() {
         rvMovies.adapter = movieAdapter
         rvMovies.layoutManager = LinearLayoutManager(this)
 
-
+        //TODO transition shared elements
+        /*val intent = Intent(this, DetailActivity::class.java)
+        // pass data object in the bundle and populate details activity.
+        //intent.putExtra(DetailActivity)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+            this@MainActivity, tvTitle as View,
+            "movieName"
+        )
+        startActivity(intent, options.toBundle())*/
 
         val client = AsyncHttpClient()
         client.get(NOW_PLAYING_URL, object: JsonHttpResponseHandler() {
--- app/src/main/java/com/example/flixster/MovieAdapter.kt
+++ app/src/main/java/com/example/flixster/MovieAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.flixster
 
 import android.content.Context
+import android.content.Intent
 import android.content.res.Configuration
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import jp.wasabeef.glide.transformations.BlurTransformation
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
 class MovieAdapter(private val context: Context, private val movies: List<Movie>)
     : RecyclerView.Adapter<MovieAdapter.ViewHolder>()
 {
@@ -27,22 +32,30 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
 
     override fun getItemCount() = movies.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
 
         private val ivPoster = itemView.findViewById<ImageView>(R.id.ivPoster)
         private val tvTitle = itemView.findViewById<TextView>(R.id.tvTitle)
         private val tvOverview = itemView.findViewById<TextView>(R.id.tvOverview)
+        private val ivPlayButton = itemView.findViewById<ImageView>(R.id.ivPlayButton)
 
+        init {
+            itemView.setOnClickListener(this)
+        }
 
         fun bind(movie: Movie) {
             tvTitle.text = movie.title
             tvOverview.text = movie.overview
+            val radius = 15;
+            val margin = 0;
 
             if (orientation == Configuration.ORIENTATION_PORTRAIT) {
                 Glide.with(context)
                     .load(movie.posterImageUrl)
                     .placeholder(R.drawable.poster_loading)
                     .error(R.drawable.poster_not_found)
+                    .centerCrop()
+                    .apply(RequestOptions.bitmapTransform(RoundedCornersTransformation(radius, margin)))
                     .into(ivPoster)
             }
             else {
@@ -50,8 +63,22 @@ class MovieAdapter(private val context: Context, private val movies: List<Movie>
                     .load(movie.backdropImageUrl)
                     .placeholder(R.drawable.poster_loading_land)
                     .error(R.drawable.poster_not_found_land)
+                    .apply(RequestOptions.bitmapTransform(RoundedCornersTransformation(radius, margin)))
                     .into(ivPoster)
             }
+            if (movie.voteAverage <= 5.0) {
+                ivPlayButton.visibility = View.INVISIBLE
+            }
+        }
+
+        override fun onClick(p0: View?) {
+            // get notified of the movie clicked
+            val movie = movies[adapterPosition]
+
+            // use the intent system to navigate to the new activity
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("MOVIE_EXTRA", movie)
+            context.startActivity(intent)
         }
     }
 }
